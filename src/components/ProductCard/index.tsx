6eb446daa32217b3a46888cd98aafd86a4f366cd
sandy <sandytian2000@hotmail.com>
2019-07-29 20:48:55 -0400

    more price stuff

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -5,9 +5,11 @@ import {
 import { compose } from 'redux';
 import { IAction } from '../../Interfaces';
 import { GetTrendsAction } from '../../actions/default';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, withStyles, Theme } from '@material-ui/core';
 import { IProduct } from '../../interfaces/Product';
 import { Record } from 'immutable';
+import { WithStyles } from '@material-ui/styles';
+import { StyleRules } from '@material-ui/core/styles';
 
 interface IProductCardComponentProps {
   productData: Record<IProduct>;
@@ -17,7 +19,7 @@ interface IProductCardProps {
   dispatch: React.Dispatch<IAction>;
 }
 
-type IProductCardType = IProductCardComponentProps & IProductCardProps;
+type IProductCardType = IProductCardComponentProps & IProductCardProps & WithStyles<keyof ReturnType<typeof styles>>;
 
 class ProductCard extends React.Component<IProductCardType, {}> {
   public componentDidMount() {
@@ -29,6 +31,7 @@ class ProductCard extends React.Component<IProductCardType, {}> {
   
   public render() {
     const {
+      classes,
       productData,
     } = this.props;
 
@@ -37,6 +40,10 @@ class ProductCard extends React.Component<IProductCardType, {}> {
     const customerRating = productData.get('customerRating') || '';
     const msrp = productData.get('msrp') || '';
     const salePrice = productData.get('salePrice') || '';
+
+    const priceContainer = salePrice
+      ? <><Typography>{salePrice}</Typography><Typography className={classes.crossedPrice}>{msrp}</   Typography></>
+      : <><Typography>{msrp}</Typography></>;
 
     return (
       <Grid
@@ -50,14 +57,20 @@ class ProductCard extends React.Component<IProductCardType, {}> {
         <Grid>
           <Typography>{shortDesc}</Typography>
           <Typography>{customerRating}</Typography>
-          <p>{msrp}</p>
+          {priceContainer}
         </Grid>
       </Grid>
     );
   }
 }
 
+const styles = (theme: Theme): StyleRules => ({
+  crossedPrice: {
+    textDecoration: 'line-through'
+  }
+})
 
 export default compose<React.ComponentClass<IProductCardComponentProps>>(
+  withStyles(styles),
   connect(),
 )(ProductCard);
