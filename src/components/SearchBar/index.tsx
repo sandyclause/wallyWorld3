65c7e077182255db9cabcb595236a46a9926affe
sandy <sandytian2000@hotmail.com>
2019-12-22 12:56:45 -0500

    search bar styles

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -9,6 +9,7 @@ import { StyleRules, WithStyles } from '@material-ui/styles';
 import walmartLogo from '../../images/walmartLogo.svg';
 import { push } from 'connected-react-router';
 import { GetSearchProducts } from '../../actions/default';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface IHeaderComponentProps {
 
@@ -55,15 +56,22 @@ class Header extends React.Component<IHeaderType, IHeaderState> {
     return (
       <Grid className={classes.container}>
         <form onSubmit={this.handleSubmit}>
-          <Input
-            placeholder="Search Product"
-            className={classes.input}
-            inputProps={{
-              'aria-label': 'Description',
-            }}
-            onChange={this.handleChange}
-          />
-          <button>Search</button>
+          <div className={classes.inputContainer}>
+            <Input
+              placeholder="Search Product"
+              className={classes.input}
+              inputProps={{
+                'aria-label': 'Description',
+              }}
+              onChange={this.handleChange}
+            />
+            <button className={classes.button}>
+              <FontAwesomeIcon
+                className={classes.searchIcon}
+                icon='search'
+              />
+            </button>
+          </div>
         </form>
       </Grid>
     );
@@ -71,7 +79,38 @@ class Header extends React.Component<IHeaderType, IHeaderState> {
 }
 
 const styles = (theme: Theme): StyleRules => ({
-
+  input: {
+    borderBottom: '1px solid',
+    borderBottomColor: 'B4B4B4',
+    border: '1px solid',
+    borderColor: '#0072CE',
+    color: '#333',
+    padding: 15,
+    width: '100%',
+    height: '100%',
+  },
+  button: {
+    width: '48px',
+    borderColor: '#0072CE',
+    backgroundColor: '#0072CE',
+    position: 'absolute',
+    right: 0,
+    top: 0,
+    bottom: 0,
+    textAlign: 'center',
+    display: 'inline-block',
+    cursor: 'pointer',
+    border: 'none',
+  },
+  inputContainer: {
+    width: '100%',
+    position: 'relative',
+  },
+  searchIcon: {
+    width: 24,
+    height: 24,
+    color: 'white',
+  }
 })
 
 export default compose<React.ComponentClass<IHeaderComponentProps>>(
